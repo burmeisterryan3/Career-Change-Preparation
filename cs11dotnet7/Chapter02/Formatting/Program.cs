7f1721d1445ac395fc6eaca2581a8716a6a70a75
Ryan Burmeister <burmeisterryan3@outlook.com>
2023-03-04 14:06:38 -0500

    Add chapter 2 files

---
+++ cs11dotnet7/Chapter02/Formatting/Program.cs
@@ -0,0 +1,67 @@
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+WriteLine(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+string formatted = string.Format(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+//WriteToFile(formatted); // writes the string into a file
+
+/* Four parameter values can use named arguments.
+WriteLine(
+    format: "{0} {1} lived in {2}, {3}.",
+    arg0: "Roger", arg1: "Cevung",
+    arg2: "Stockholm", arg3: "Sweden");
+*/
+
+// Five or more parameter values cannot use named arguments.
+WriteLine(
+    format: "{0} {1} lived in {2}, {3} and worked in the {4} team at {5}.",
+    "Roger", "Cevung", "Stockholm", "Sweden", "Education", "Optimizely");
+
+// The following statement must be all on one line... Can separate by newlines in C# 11
+WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}");
+
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+WriteLine(
+    format: "{0,-10} {1,6}",
+    arg0: "Name",
+    arg1: "Count");
+
+WriteLine(
+    format: "{0,-10} {1,6:N0}",
+    arg0: applesText,
+    arg1: applesCount);
+
+WriteLine(
+    format: "{0,-10} {1,6:N0}",
+    arg0: bananasText,
+    arg1: bananasCount);
+
+// ReadLine functionality
+Write("Type your first name and press ENTER: ");
+string firstName = ReadLine()!;
+
+Write("Type your age and press ENTER: ");
+string? age = ReadLine();
+
+WriteLine($"Hello {firstName}, you look good for {age}.");
+
+// ReadKey
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key,
+    arg1: key.KeyChar,
+    arg2: key.Modifiers);
---
+++ cs11dotnet7/Chapter02/Vocabulary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+// Decleare unused variables using types in additional assemblies
+// Will force additional assemblies to be loaded
+System.Data.DataSet ds;
+HttpClient client;
+
+Assembly? myApp = Assembly.GetEntryAssembly();
+
+if (myApp == null) return; // quit the app
+
+// Loop through the assemblies that my app references
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    // Load the assembly so we can read its details
+    Assembly a = Assembly.Load(name);
+
+    // Declare a variable to count the number of methods
+    int methodCount = 0;
+
+    // Loop through all the types in the assembly
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        // Add up the counts of methods
+        methodCount += t.GetMethods().Length;
+    }
+
+    // Output the Count of types and their methods
+    Console.WriteLine(
+        "{0:N0} types with {1:N0} methods in {2} assembly.",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount,
+        arg2: name.Name);
+}
