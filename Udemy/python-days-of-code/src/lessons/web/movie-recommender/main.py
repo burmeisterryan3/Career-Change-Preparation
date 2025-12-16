58df79c3c7141a6c028aa81dbd473d88d730d600
burmeisterryan3 <ryan.j.burmeister@outlook.com>
2025-12-15 22:57:35 -0500

    Clean up day 45 challenge

--- Udemy/python-days-of-code/src/lessons/web/movie-recommender/main.py
+++ Udemy/python-days-of-code/src/lessons/web/movie-recommender/main.py
@@ -4,21 +4,20 @@ from bs4 import BeautifulSoup
 import requests
 
 OUTPUT_FILE = "./movies.txt"
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+SELECTOR = "div.article-title-description__text h3.title"
 
 
 def main():
     """Main logic."""
-    response = requests.get(
-        "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
-    )
+    response = requests.get(URL)
     soup = BeautifulSoup(response.text, "html.parser")
 
-    movies = [
-        movie.getText() for movie in soup.select("div.article-title-description__text h3.title")
-    ]
+    movies = [movie.getText() for movie in soup.select(SELECTOR)]
 
     with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
         for movie in reversed(movies):
+            # Clean up this particular entry which is mislabeled
             if movie.split()[0] == "15)":
                 movie = list(movie)
                 movie[:3] = "80)"
